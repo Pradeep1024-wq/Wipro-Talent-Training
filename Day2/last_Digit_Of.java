2343a7d0832062cb6d220fdb0a30a3419dc9b6bf
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2023-02-07 14:44:37 +0530

    Create last_Digit_Of.java

---
+++ Day2/last_Digit_Of.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import  java.util.*;
+class last_Digit_Of
+{
+ public int lastDigitOf(int input1){
+     if(input1<0)
+  {
+   input1*=-1;
+  }
+  int n=input1%10;
+  return n;
+ }
